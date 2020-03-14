b530f8082667e754caf6f2316283af478d5141c1
Nikita Bobers <58427328+nikitabobers@users.noreply.github.com>
2020-03-14 14:14:12 +0100

    code clean

--- app.js
+++ app.js
@@ -8,21 +8,19 @@ const yearNow = new Date().getFullYear();
 const yearNew = new Date(`January 01 ${yearNow + 1} 00:00:00`);
 
 function countdownTimer() {
-  const timeNow = new Date();
-  const difference = yearNew - timeNow;
-  // console.log(difference);
+    const timeNow = new Date();
+    const difference = yearNew - timeNow;
+    const daysLeft = Math.floor(difference / 1000 / 60 / 60 / 24);
+    const hoursLeft = Math.floor((difference / 1000 / 60 / 60) % 24);
+    const minLeft = Math.floor((difference / 1000 / 60) % 60);
+    const secondLeft = Math.floor((difference / 1000) % 60);
 
-  const daysLeft = Math.floor(difference / 1000 / 60 / 60 / 24);
-  const hoursLeft = Math.floor((difference / 1000 / 60 / 60) % 24);
-  const minLeft = Math.floor((difference / 1000 / 60) % 60);
-  const secondLeft = Math.floor((difference / 1000) % 60);
+    days.innerHTML = daysLeft;
+    hours.innerHTML = hoursLeft < 10 ? "0" + hoursLeft : hoursLeft;
+    min.innerHTML = minLeft < 10 ? "0" + minLeft : minLeft;
+    second.innerHTML = secondLeft < 10 ? "0" + secondLeft : secondLeft;
 
-  days.innerHTML = daysLeft;
-  hours.innerHTML = hoursLeft < 10 ? "0" + hoursLeft : hoursLeft;
-  min.innerHTML = minLeft < 10 ? "0" + minLeft : minLeft;
-  second.innerHTML = secondLeft < 10 ? "0" + secondLeft : secondLeft;
-
-  year.innerHTML = yearNow;
+    year.innerHTML = yearNow;
 }
 
 countdownTimer();
